5527a6e53ae99dc6db8c40524089d147c4f2e9c6
Damian Kluk <damian.kluk.92@gmail.com>
2019-10-25 12:52:29 +0200

    Controller with routes

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -2,20 +2,82 @@
 
 namespace App\Http\Controllers;
 
+use App\Event;
 use App\Repositories\EventRepository;
+use App\Rules\EventDuration;
+use App\Rules\EventOverlaps;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class EventController extends Controller
 {
-    public function index(string $date = 'NOW')
+    public function index(string $date = 'NOW', EventRepository $eventRepository)
     {
-        $validator = Validator::make(['date' => $date], ['date' => 'required|datetime'])->validate();
-
         $dateTime = new \DateTime($date);
 
-        $events = EventRepository::getByDate($dateTime);
+        $date = $dateTime->format('Y-m-d');
+
+        $validator = Validator::make(['date' => $date], [
+            'date' => 'required|date'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $slots = [
+            'openDate' => $dateTime->format('Y-m-d 08:00:00'),
+            'closeDate' => $dateTime->format('Y-m-d 20:00:00'),
+            'duration' => Event::DURATION_IN_MINUTES
+        ];
 
         return response()->json([
-            'events' => $events
+            'events' => $eventRepository->getByDate($slots['openDate'], $slots['closeDate']),
+            'slots' =>$slots
         ]);
+    }
+
+    public function show(Event $event)
+    {
+        return $event;
+    }
+
+    public function store(Request $request, EventRepository $eventRepository)
+    {
+        $validator = Validator::validate($request->all(), [
+            'start' => ['required|datetime|before:end', new EventOverlaps($request->get('end'))],
+            'end' => ['required|datetime', new EventDuration($request->get('start'))]
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $event = Event::create($request->all());
+
+        return response()->json($event, 201);
+    }
+
+    public function update(Request $request, Event $event)
+    {
+        $validator = Validator::validate($request->all(), [
+            'start' => ['required|datetime', new EventOverlaps($request->get('end'))],
+            'end' => ['required|datetime', new EventDuration($request->get('start'))]
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $event->update($request->all());
+
+        return response()->json($event, 200);
+    }
+
+    public function delete(Event $event)
+    {
+        $event->delete();
+
+        return response()->json(null, 204);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,7 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/events/{date?}', 'EventController@index');
+Route::get('events', 'EventController@index');
+Route::get('events/{event}', 'EventController@show');
+Route::post('events', 'EventController@store');
+Route::put('events/{event}', 'EventController@update');
+Route::delete('events/{event}', 'EventController@delete');
